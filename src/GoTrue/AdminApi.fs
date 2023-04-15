8b3f44f7db0d51ad3e44f5958d5d710a6d0aa6be
Samo8 <samodubovec@gmail.com>
2023-04-15 16:42:23 +0200

    fix: fixed return type from UserResponse to User

--- src/GoTrue/AdminApi.fs
+++ src/GoTrue/AdminApi.fs
@@ -1,5 +1,6 @@
 namespace GoTrue
 
+open System
 open System.Net.Http
 open System.Text
 open FSharp.Json
@@ -39,18 +40,34 @@ module AdminApiHelpers =
         match value with
         | Some v -> map |> Map.add key v
         | _      -> map
+        
+    let linkTypeToSnakeCase (linkType: GenerateLinkType): string =
+        let rec loop lt acc =
+            match lt with
+            | "" -> acc
+            | _ ->
+                let c, rest = lt[0], lt[1..]
+                if Char.IsUpper c then
+                    let acc' = if acc = "" || acc.EndsWith("_") then acc + (Char.ToLowerInvariant c).ToString() else acc + "_" + (Char.ToLowerInvariant c).ToString()
+                    loop rest acc'
+                else
+                    loop rest (acc + string c)
+
+        loop (linkType.ToString()) ""
+        
+    let userToUserResponse (user: Result<User, GoTrueError>): Result<UserResponse, GoTrueError> =
+        match user with
+        | Ok u    -> Ok { user = Some u }
+        | Error e -> Error e
 
 [<AutoOpen>]
 module AdminApi =
-    let signOut (connection: GoTrueConnection): Result<unit, GoTrueError> =
-        performAuthRequest None None [] "logout" None connection deserializeEmptyResponse
-    
-    let createUser (attributes: AdminUserAttributes) (connection: GoTrueConnection): Result<UserResponse, GoTrueError> =
+    let createUser (attributes: AdminUserAttributes) (connection: GoTrueConnection): Result<User, GoTrueError> =
         let body = Json.serialize attributes
         let content = getStringContent body
         
         let response = post "admin/users" None content connection
-        deserializeResponse<UserResponse> response
+        deserializeResponse<User> response
         
     let deleteUser (id: string) (connection: GoTrueConnection): Result<unit, GoTrueError> =
         let urlSuffix = $"admin/users/{id}"
@@ -76,36 +93,35 @@ module AdminApi =
         deserializeResponse<User list> response
         
     let inviteUserByEmail (email: string) (options: AuthOptions option)
-                          (connection: GoTrueConnection): Result<UserResponse, GoTrueError> =
+                          (connection: GoTrueConnection): Result<User, GoTrueError> =
         let body = Map<string, obj>[ "email", email ]
         
-        performAuthRequest (Some body) None [] "invite" options connection deserializeResponse<UserResponse>
+        performAuthRequest (Some body) None [] "invite" options connection deserializeResponse<User>
         
-    let getUserById (uid: string) (connection: GoTrueConnection): Result<UserResponse, GoTrueError> =
+    let getUserById (uid: string) (connection: GoTrueConnection): Result<User, GoTrueError> =
         let urlSuffix = $"admin/users/{uid}"
         
         let response = get urlSuffix None connection
-        deserializeResponse<UserResponse> response
+        deserializeResponse<User> response
         
     let updateUserById (uid: string) (attributes: AdminUserAttributes)
-                       (connection: GoTrueConnection): Result<UserResponse, GoTrueError> =
+                       (connection: GoTrueConnection): Result<User, GoTrueError> =
         let urlSuffix = $"admin/users/{uid}"
         
         let body = Json.serialize attributes
         let content = getStringContent body
         
         let response = put urlSuffix None content connection
-        deserializeResponse<UserResponse> response
+        deserializeResponse<User> response
     
     let generateLink (email: string) (linkType: GenerateLinkType) (options: AuthOptions option) (password: string option)
-                     (data: Map<string, obj> option) (connection: GoTrueConnection): Result<UserResponse, GoTrueError> =
-        //TODO: linkType to snake_case 
+                     (data: Map<string, obj> option) (connection: GoTrueConnection): Result<User, GoTrueError> =
         let body =
             Map<string, obj>[
                 "email", email
-                "type", linkType
+                "type", linkTypeToSnakeCase linkType
             ]
             |> addItemToMapIfPresent "data" data
             |> addItemToMapIfPresent "password" password
               
-        performAuthRequest (Some body) None [] "admin/generate_lin" options connection deserializeResponse<UserResponse>
+        performAuthRequest (Some body) None [] "admin/generate_link" options connection deserializeResponse<User>
