bcb638328888720bc9c68e97ae623f6ba41da675
Samo8 <samodubovec@gmail.com>
2023-04-15 12:06:00 +0200

    feat: updated Http module

--- src/GoTrue/Http.fs
+++ src/GoTrue/Http.fs
@@ -22,7 +22,7 @@ module Http =
         match response with
         | Ok r    ->
             printfn $"{r.RequestMessage}"
-            Result.Ok (Json.deserialize<'T> (r |> getResponseBody))
+            Result.Ok (Json.deserialize<'T> (getResponseBody r))
         | Error e -> Result.Error e
         
     let deserializeEmptyResponse (response: Result<HttpResponseMessage, GoTrueError>): Result<unit, GoTrueError> =
@@ -30,65 +30,68 @@ module Http =
         | Ok _    -> Result.Ok ()
         | Error e -> Result.Error e
         
-    let executeHttpRequest (headers: Map<string, string> option) (requestMessage: HttpRequestMessage)
-                           (connection: GoTrueConnection): Result<HttpResponseMessage, GoTrueError> =
+    let executeHttpRequest (requestMessage: HttpRequestMessage) (connection: GoTrueConnection): Result<HttpResponseMessage, GoTrueError> =
         try
             let httpClient = connection.HttpClient
             let result =
                 task {
-                    addRequestHeaders connection.Headers requestMessage.Headers
-                    
-                    match headers with
-                    | Some h -> addRequestHeaders h requestMessage.Headers
-                    | _      -> ()
+                    let connectionHeaders =
+                        match requestMessage.Headers.Contains "Authorization" &&
+                              connection.Headers.ContainsKey "Authorization" with
+                        | true -> connection.Headers.Remove "Authorization"
+                        | false -> connection.Headers
+                    addRequestHeaders connectionHeaders requestMessage.Headers
                     
                     let response = httpClient.SendAsync(requestMessage)
                     return! response
                 } |> Async.AwaitTask |> Async.RunSynchronously
             match result.StatusCode with
             | HttpStatusCode.OK -> Result.Ok result
-            | statusCode        ->
-                Result.Error { message    = getResponseBody result
-                               statusCode = Some statusCode }
-        with e ->
-            Result.Error { message    = e.ToString()
-                           statusCode = None }
+            | statusCode        -> Result.Error { message    = getResponseBody result
+                                                  statusCode = Some statusCode }
+        with e -> Result.Error { message    = e.ToString()
+                                 statusCode = None }
             
-    let private getRequestMessage (httpMethod: HttpMethod) (url: string) (urlSuffix: string): HttpRequestMessage =
-        new HttpRequestMessage(httpMethod, $"{url}/{urlSuffix}")
+    let private getRequestMessage (httpMethod: HttpMethod) (url: string) (urlSuffix: string)
+                                  (headers: Map<string, string> option): HttpRequestMessage =
+        let requestMessage = new HttpRequestMessage(httpMethod, $"{url}/{urlSuffix}")
+        match headers with
+        | Some h -> addRequestHeaders h requestMessage.Headers
+        | _      -> ()
+        requestMessage
         
     let get (urlSuffix: string) (headers: Map<string, string> option)
             (connection: GoTrueConnection): Result<HttpResponseMessage, GoTrueError> =
-        let requestMessage = getRequestMessage HttpMethod.Get connection.Url urlSuffix
+        let requestMessage = getRequestMessage HttpMethod.Get connection.Url urlSuffix headers
 
-        executeHttpRequest headers requestMessage connection
+        executeHttpRequest requestMessage connection
         
     let delete (urlSuffix: string) (headers: Map<string, string> option) (content: HttpContent option)
                (connection: GoTrueConnection): Result<HttpResponseMessage, GoTrueError> =
-        let requestMessage = getRequestMessage HttpMethod.Delete connection.Url urlSuffix
+        let requestMessage = getRequestMessage HttpMethod.Delete connection.Url urlSuffix headers
         match content with
         | Some c -> requestMessage.Content <- c
         | _      -> ()
         
-        executeHttpRequest headers requestMessage connection 
+        executeHttpRequest requestMessage connection 
     
     let post (urlSuffix: string) (headers: Map<string, string> option) (content: StringContent)
              (connection: GoTrueConnection): Result<HttpResponseMessage, GoTrueError> =
-        let requestMessage = getRequestMessage HttpMethod.Post connection.Url urlSuffix
+        let requestMessage = getRequestMessage HttpMethod.Post connection.Url urlSuffix headers
         requestMessage.Content <- content
         
-        executeHttpRequest headers requestMessage connection 
+        executeHttpRequest requestMessage connection 
             
     let patch (urlSuffix: string) (headers: Map<string, string> option) (content: StringContent)
               (connection: GoTrueConnection): Result<HttpResponseMessage, GoTrueError> =
-        let requestMessage = getRequestMessage HttpMethod.Patch connection.Url urlSuffix
+        let requestMessage = getRequestMessage HttpMethod.Patch connection.Url urlSuffix headers
         requestMessage.Content <- content
         
-        executeHttpRequest headers requestMessage connection
+        executeHttpRequest requestMessage connection
         
     let put (urlSuffix: string) (headers: Map<string, string> option) (content: StringContent)
              (connection: GoTrueConnection): Result<HttpResponseMessage, GoTrueError> =
-        let requestMessage = getRequestMessage HttpMethod.Put connection.Url urlSuffix
+        let requestMessage = getRequestMessage HttpMethod.Put connection.Url urlSuffix headers
         requestMessage.Content <- content
         
-        executeHttpRequest headers requestMessage connection 
+        executeHttpRequest requestMessage connection 
