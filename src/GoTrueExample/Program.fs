1e1ea93efde8a0a7bda052660bc32613a40b9259
Samo8 <samodubovec@gmail.com>
2023-04-25 12:42:14 +0200

    feat: updated example file

--- src/GoTrueExample/Program.fs
+++ src/GoTrueExample/Program.fs
@@ -1,2 +1,16 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿open GoTrue
+
+let baseUrl = "https://<project-id>.supabase.co/auth/v1"
+let apiKey = "<api-key>"
+
+let connection = goTrueConnection {
+     url baseUrl
+     headers (Map [ "apiKey", apiKey
+                    "Authorization", $"Bearer {apiKey}" ]
+     )
+}
+
+let result = signInWithEmail "<email>" "<password>" None connection
+match result with
+| Ok r    -> printf $"{r}"
+| Error e -> printfn $"{e}"
